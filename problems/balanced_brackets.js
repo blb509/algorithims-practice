2defde6e6c045bcb5e4fdac86e5598627efce94f
Brandon L. Borrero <borrero509@outlook.com>
2020-02-12 01:33:45 -0800

    solutions

--- problems/balanced_brackets.js
+++ problems/balanced_brackets.js
@@ -1,3 +1,32 @@
 // Write a function that takes a string as input. The string can contain four types of brackets: "[]", "()", "{}", and "||".
 
 // Your function should return a boolean indicating wether or not the string is balanced.
+
+function balancedBrackets(string) {
+  let stack = [];
+  let object = {
+    "[": "]",
+    "(": ")",
+    "{": "}",
+    "|": "|"
+  };
+  let openBrackets = ["[", "(", "{", "|"];
+  let closedBrackets = ["]", ")", "}", "|"];
+  let str = string.split(" ").join("");
+  let removing = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (openBrackets.includes(str.charAt(i)) && removing == 0) {
+      stack.push(str.charAt(i));
+    } else if (closedBrackets.includes(str.charAt(i))) {
+      let popped = stack.pop;
+      removing = 1;
+      if (object[popped] != str.charAt(i)) {
+        return 0;
+      }
+    }
+  }
+  if (stack.length > 0) {
+    return 0;
+  }
+  return 1;
+}
--- problems/three_numberSum.js
+++ problems/three_numberSum.js
@@ -2,4 +2,29 @@
 
 // should find all triplets in the array that sum up to the target sum and return a two dimensional array
 
-// of all these triplets. Each innerarray containing a single triplet should be sorted in ascending order.
+// of all these triplets. Each inner array containing a single triplet should be sorted in ascending order.
+
+function threeNumberSum(arr, target) {
+  if (arr.length < 3) {
+    return [];
+  }
+  arr.sort((a, c) => a - b);
+  let output = [];
+  for (let i = 0; i <= arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right) {
+      let sum = arr[i] + arr[left] + arr[right];
+      if (sum > target) {
+        right--;
+      } else if (sum < target) {
+        left++;
+      } else {
+        output.push([arr[i], arr[left], arr[right]]);
+        left++;
+        right--;
+      }
+    }
+  }
+  return output;
+}
